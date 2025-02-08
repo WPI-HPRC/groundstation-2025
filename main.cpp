072b7d0c10f292c480eb54a8b7633bebdbdfd4ec
Will Scheirey <williamscheirey@gmail.com>
2025-02-08 15:39:52 -0500

    run direwolf directly within the app!
    NOTE: this requires direwolf and rtl_fm to be installed on your computer

--- main.cpp
+++ main.cpp
@@ -1,16 +1,48 @@
 
 #include <QApplication>
+#include <QProcess>
 #include "Backend/Backend.h"
 #include "Frontend/Windows/MainWindow/mainwindow.h"
 #include "Backend/KissClient.h"
+#include <QTimer>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    KissClient kissClient;
-//    kissClient.connectToDireWolf(QUrl("ws://localhost:8001")); // Dire Wolf's KISS TCP port
+    QProcess process(&a);
+    process.setProgram("bash");
 
+    QFile script(":/APRS/run_aprs.sh");
+    if (!script.open(QIODevice::ReadOnly | QIODevice::Text))
+        return 1;
+    QString scriptContents = script.readAll();
+
+    QFile direWolfConf(":/APRS/direwolf.conf");
+    if (!direWolfConf.open(QIODevice::ReadOnly | QIODevice::Text))
+        return 1;
+
+    QString confContents = direWolfConf.readAll();
+    scriptContents.replace("direwolf.conf", "/dev/stdin");
+
+    qDebug() << scriptContents;
+
+    process.setArguments(QStringList() << "-c" << scriptContents);
+    process.start();
+    process.write(confContents.toUtf8());
+    process.closeWriteChannel();
+
+    QObject::connect(&process, &QProcess::readyReadStandardOutput, [&]() {
+        std::cout << process.readAllStandardOutput().toStdString();
+    });
+
+    QObject::connect(&process, &QProcess::readyReadStandardError, [&]() {
+        qDebug() << "Error:" << process.readAllStandardError();
+    });
+
+    QTimer::singleShot(1000, [](){
+        KissClient kissClient;
+    });
 
     QFont font("Courier New");
     font.setStyleHint(QFont::Monospace);
@@ -22,7 +54,7 @@ int main(int argc, char *argv[])
 
     Backend &backend = Backend::getInstance();
     backend.start();
-    
+
     int code = QApplication::exec();
 
     backend.flushFiles();
